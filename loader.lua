181a9f9072d14ca95bc2b1c41c8eb74329664bfe
ZenithDev0Br <zenithdev.br@gmail.com>
2026-07-02 09:32:41 -0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,26 @@
+-- Zenith-Hub/loader.lua
+-- Loader global do Zenith Hub
+-- Responsabilidade: identificar o jogo atual e carregar o módulo correspondente.
+
+local GameId = game.PlaceId
+
+local GameModules = {
+    [2753915549] = "bloxfruits", -- Blox Fruits (ID principal)
+    [4483381587] = "bloxfruits", -- Blox Fruits (ID alternativo)
+}
+
+local GameKey = GameModules[GameId]
+
+if not GameKey then
+    warn("[Zenith Hub] Jogo não suportado: " .. tostring(GameId))
+    return
+end
+
+-- Carrega o loader específico do jogo
+local GameLoader = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/ZenithHub/main/" .. GameKey .. "/loader.lua"
+))()
+
+if GameLoader then
+    GameLoader.Start()
+end
